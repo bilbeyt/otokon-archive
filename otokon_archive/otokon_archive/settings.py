2a7e81c59f4489e94cc568661db56eeedcf63bb9
alperkesen <kesen.alper@gmail.com>
2017-11-17 02:14:43 -0500

    file uploading problem solved

--- otokon_archive/otokon_archive/settings.py
+++ otokon_archive/otokon_archive/settings.py
@@ -141,7 +141,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.abspath(
+	os.path.join(BASE_DIR, os.pardir, "public", "media"))
 
 CKEDITOR_IMAGE_BACKEND = "pillow"
 
